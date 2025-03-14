5cb973b5adaa882a8ff14fb0e6e31a3f15178c9c
Leeamin-1 <oegyeinn@gmail.com>
2025-03-14 14:21:41 +0900

    Initial commit

---
+++ src/main/java/Hello.java
@@ -0,0 +1,13 @@
+public class Hello {
+
+    public static void main(String[] args){
+
+        System.out.println("Hello!! My First Java Coding!!" );
+        System.out.println("Hello!! My Second Java Coding!!" );
+        System.out.println("Hello!! My Third Java Coding!!" );
+        System.out.println("Hello!! My Fourth Java Coding!!" );
+        System.out.println("Hello!! My Fifth Java Coding!!" );
+        System.out.println("Hello!! My Sixth Java Coding!!" );
+        System.out.println("Hello!! My Seventh Java Coding!!" );
+    }
+}
