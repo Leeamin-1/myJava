d8fbd8684963d268bd9c592013264857e9adfcf1
Leeamin-1 <oegyeinn@gmail.com>
2025-03-14 14:57:47 +0900

    변경

---
+++ src/main/java/Hello5.java
@@ -0,0 +1,7 @@
+public class Hello5 {
+    public static void main(String[] args) {
+        System.out.println("2025y 03M 14D");
+        System.out.println("friday 소프트부트프로그래밍 수업");
+        System.out.println("오후 2시 57분");
+    }
+}
