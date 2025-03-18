b6ec42a1ed73847f85fc1c3aff89c5f70d360846
Leeamin-1 <oegyeinn@gmail.com>
2025-03-18 13:16:42 +0900

    변경

--- src/main/java/Subject1000.java
+++ src/main/java/Subject1000.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Subject1000 {
 
-public static void main(String[] args) {
+   public static void main(String[] args) {
 
     Scanner in = new Scanner(System.in);
     int A =  in.nextInt();
--- src/main/java/Subject1001.java
+++ src/main/java/Subject1001.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Subject1001 {
 
-public static void main(String[] args) {
+    public static void main(String[] args) {
 
     Scanner in = new Scanner(System.in);
     int A = in.nextInt();
@@ -11,6 +11,6 @@ public static void main(String[] args) {
     System.out.println(A-B);
 
     in.close();
-  }
+    }
 }
 
