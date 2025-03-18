a2c56571192c23e288afea51d5ae7506b3683055
Leeamin-1 <oegyeinn@gmail.com>
2025-03-18 13:06:50 +0900

    변경

---
+++ src/main/java/Subject1008.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Subject1008 {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        double A = in.nextInt();
+        double B = in.nextInt();
+
+        System.out.println(A/B);
+
+        in.close();
+    }
+}
