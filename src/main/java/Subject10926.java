f78d185806da534e52bc1ce2ddd0be0102780fdc
Leeamin-1 <oegyeinn@gmail.com>
2025-03-19 12:59:42 +0900

    변경

---
+++ src/main/java/Subject10926.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Subject10926 {
+
+    public static void main(String[] args) {
+
+        Scanner in =  new Scanner(System.in);
+
+        String laststring = "??!";
+
+        String s = in.next();
+        System.out.println(s + laststring);
+    }
+}
