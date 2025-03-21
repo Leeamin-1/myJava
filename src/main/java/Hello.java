2df2639dc097b8c0c925f48f53ffcf0f996b919d
Leeamin-1 <oegyeinn@gmail.com>
2025-03-21 12:36:59 +0900

    변경

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,14 +2,6 @@ public class Hello {
 
     public static void main(String[] args){
 
-        System.out.println("Hello!! My First Java Coding!!" );
-        System.out.println("Hello!! My Second Java Coding!!" );
-        System.out.println("Hello!! My Third Java Coding!!" );
-        System.out.println("Hello!! My Fourth Java Coding!!" );
-        System.out.println("Hello!! My Fifth Java Coding!!" );
-        System.out.println("Hello!! My Sixth Java Coding!!" );
-        System.out.println("Hello!! My Seventh Java Coding!!" );
-        System.out.println("Edit Java Code");
-
+        System.out.println("안녕하세요!" );
     }
 }
--- src/main/java/Subject2557.java
+++ src/main/java/Subject2557.java
@@ -1,4 +1,4 @@
-public class Subject2557 {
+public class                                                                                                                         Subject2557 {
 
     public static void main(String[] args) {
         System.out.println("Hello World!" );
