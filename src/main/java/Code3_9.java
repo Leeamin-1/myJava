c2170bb9e377ce3dedad8b85069154d504922287
Leeamin-1 <oegyeinn@gmail.com>
2025-03-28 13:22:06 +0900

    변경

---
+++ src/main/java/Code3_9.java
@@ -0,0 +1,12 @@
+public class Code3_9 {
+    public static void main(String[] args) {
+        int num = 20;
+        num++ ; System.out.println(num+" ");
+        num-- ; System.out.println(num+" ");
+        num += 3 ; System.out.println(num+" ");
+        num -= 3 ; System.out.println(num+" ");
+        num *= 3 ; System.out.println(num+" ");
+        num /= 3 ; System.out.println(num+" ");
+        num %= 3 ; System.out.println(num+" ");
+    }
+}
