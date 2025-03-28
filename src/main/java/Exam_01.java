345d0ba3403155fc1d1f0914673bdcbfba419d8b
Leeamin-1 <oegyeinn@gmail.com>
2025-03-28 13:10:40 +0900

    변경

--- src/main/java/Exam_01.java
+++ src/main/java/Exam_01.java
@@ -1,20 +1,17 @@
 import java.util.Scanner;
-
-public class Exam_01 {
-
-    public static void main(String[] args) {
+public class Exam_01{
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.println("숫자를 입력해 주세요: ");
+
+        System.out.print("숫자를 입력하시오: ");
         int a = sc.nextInt();
 
-        System.out.println("숫자를 입력해 주세요: ");
+        System.out.print("숫자를 입력하시오: ");
         int b = sc.nextInt();
 
-        double result = (double) a / b;
+        double result =(double)a / b;
+
         System.out.println(a + " / " + b + " = " + result);
-
         sc.close();
-
-
     }
 }
