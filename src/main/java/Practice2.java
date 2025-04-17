d89811ff9b62bd72b88ea0557447ffe73b05bed9
Leeamin-1 <oegyeinn@gmail.com>
2025-04-17 16:34:06 +0900

    변경

---
+++ src/main/java/Practice2.java
@@ -0,0 +1,8 @@
+public class Practice2 {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i + "번째 반복");
+        }
+    }
+}
---
+++ src/main/java/Practice3.java
@@ -0,0 +1,8 @@
+public class Practice3 {
+    public static void main(String[] args) {
+
+        for (int i = 5; i >= 1; i--){
+            System.out.println(i + "번 말할거야");
+        }
+    }
+}
